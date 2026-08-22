2ffbee94824577cf826a244cf57ebd85f5548c86
Ayaz Computer <ayazilyas047@gmail.com>
2026-08-22 03:12:09 -0700

    Upadet Code

--- index.php
+++ index.php
@@ -5,7 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<style>
+    body {
+        background: black;
+        color: white;
+        font-weight: 700;
+        font-size: 30px;
+    }
+</style>
+
 <body>
     
+<?php 
+
+$a = 5;
+$b = 2;
+
+echo $a + $b;
+
+
+?>
+
+
+
 </body>
 </html>
